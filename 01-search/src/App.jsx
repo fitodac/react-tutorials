8ffdf2931fcb496713692be3dddfe343a22cb1e7
fitodac <fitodac@gmail.com>
2023-10-02 07:48:21 -0300

    Search main files

---
+++ 01-search/src/App.jsx
@@ -0,0 +1,13 @@
+import Search from './components/Search'
+
+function App() {
+
+	return (
+		<main className="flex justify-center p-6 pb-20">
+			{/* Se muestra el componente Search */}
+			<Search />
+		</main>
+	)
+}
+
+export default App
