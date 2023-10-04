8578bd67c47fb957a48e45ac19dffed516502658
fitodac <fitodac@gmail.com>
2023-10-04 17:41:07 -0300

    app pages

---
+++ 03-auth/src/app/pages/Page1.jsx
@@ -0,0 +1,13 @@
+export const Page1 = () => {
+	return (
+		<>
+			<div className="text-xl">Page 1</div>
+
+			<div className="mt-8 space-y-7">
+				<p>{`Don't be too proud of this technological terror you’ve constructed. The ability to destroy a planet is insignificant next to the power of the Force. Luke, you can destroy the Emperor. He has foreseen this. It is your destiny! Join me, and together, we can rule the galaxy as father and son! Come with me. It is the only way`}</p>
+				<p>{`The dark side of the Force is a pathway to many abilities some consider to be unnatural. Give yourself to the dark side. It is the only way you can save your friends. Yes, your thoughts betray you. Your feelings for them are strong. You need a teacher. I could show you the ways of the force.`}</p>
+				<p>{`I am altering the deal. Pray, I don't alter it any further. Forgive me. I feel it again... the call from light. There is no escape! Don't make me destroy you. Luke, you do not yet realize your importance. You've only begun to discover your power! Join me, and I will compconste your training! With our combined strength, we can end this destructive conflict, and bring order to the galaxy.`}</p>
+			</div>
+		</>
+	)
+}
---
+++ 03-auth/src/app/pages/Page2.jsx
@@ -0,0 +1,14 @@
+export const Page2 = () => {
+	return (
+		<>
+			<div className="text-xl">Page 2</div>
+
+			<div className="mt-8 space-y-7">
+				<p>{`Those who do not serve my ends, no matter how powerful they are, will be eliminated. So, you have a twin sister. Your feelings have now betrayed her too. Obi-Wan was wise to hide her from me. Now his failure is compconste. If you will not turn to the dark side, then perhaps she will.`}</p>
+				<p>{`Forgive me. I feel it again... the call from light. The Emperor does not share your optimistic appraisal of the situation. By the grace of your training, I will not be seduced. You are a part of the Rebel Alliance and a traitor! It is...too late for me, son. The Emperor will show you the true nature of the Force. He is your master now.`}</p>
+				<p>{`You are a part of the Rebel Alliance and a traitor! Search your feelings; you know it to be true! You must break through the fog of lies the Jedi have created around you. const me help you to know the subtconsties of the Force. Nothing will stand in our way. I will finish what you started.`}</p>
+				<p>{`We have a new enemy. The young Rebel who destroyed the Death Star. I have no doubt this boy is the offspring of Anakin Skywalker. I'm being torn apart. I want to be free of this pain. I know what I have to do, but I don't know if I have the strength to do it. Will you help me?`}</p>
+			</div>
+		</>
+	)
+}
